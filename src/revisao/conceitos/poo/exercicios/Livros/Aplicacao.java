019def7d0d5b0a0668c9ae1d7517d052cb2bc7fa
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-10-14 15:00:28 -0300

    Atividade sobre modificadores de acesso

---
+++ src/revisao/conceitos/poo/exercicios/Livros/Aplicacao.java
@@ -0,0 +1,14 @@
+package revisao.conceitos.poo.exercicios.Livros;
+
+public class Aplicacao {
+    public static void main(String[] args) {
+
+        Livro livro1 = new Livro();
+        Livro livro2 = new Livro("A hora da estrela","Clarice Lispector");
+        Livro livro3 = new Livro("Vidas Secas","Graciliano Ramos",2000,53.99f);
+
+        livro1.exibirLivro();
+        livro2.exibirLivro();
+        livro3.exibirLivro();
+    }
+}
---
+++ src/revisao/conceitos/poo/exercicios/Livros/Livro.java
@@ -0,0 +1,30 @@
+package revisao.conceitos.poo.exercicios.Livros;
+
+public class Livro {
+    public String titulo;
+    private String autor;
+    protected int anoPublicacao;
+    float preco;
+
+    public Livro() {
+    }
+
+    public Livro(String titulo, String autor, int anoPublicacao, float preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+        this.preco = preco;
+    }
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+    public void exibirLivro(){
+        System.out.println("------------------------------------");
+        System.out.println("Titulo: "+titulo);
+        System.out.println("Autor: "+autor);
+        System.out.println("Ano de publicacao: "+anoPublicacao);
+        System.out.println("Preco: "+preco);
+    }
+}
