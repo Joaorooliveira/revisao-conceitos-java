ca9631c505ce09ea76c329b1ce5a7381748c58c8
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-10-14 15:21:40 -0300

    Atividade sobre modificadores de acesso2

---
+++ src/revisao/conceitos/poo/exercicios/DadosPessoa/Aplicacao.java
@@ -0,0 +1,31 @@
+package revisao.conceitos.poo.exercicios.DadosPessoa;
+
+public class Aplicacao {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+        p1.nome = "Joao";
+        p1.setIdade(25);
+        p1.setEndereco("Qnl 20 bloco f apt 50");
+        p1.setTelefone(618176526);
+
+        Pessoa p2 = new Pessoa();
+        p2.nome = "Maria";
+        p2.setIdade(30);
+        p2.setEndereco("Qnl 30 bloco D apt 12");
+        p2.setTelefone(618321321);
+
+        System.out.println("Nome: " + p1.nome);
+        System.out.println("Idade: "+p1.getIdade());
+        System.out.println("Endereco :"+p1.getEndereco());
+        System.out.println("Telefone :"+p1.getTelefone());
+        System.out.println("--------------------------------------");
+        System.out.println("Nome: " + p2.nome);
+        System.out.println("Idade: "+p2.getIdade());
+        System.out.println("Endereco :"+p2.getEndereco());
+        System.out.println("Telefone :"+p2.getTelefone());
+
+        /*p1.exibirPessoa();
+        p2.exibirPessoa();*/
+
+    }
+}
---
+++ src/revisao/conceitos/poo/exercicios/DadosPessoa/Pessoa.java
@@ -0,0 +1,40 @@
+package revisao.conceitos.poo.exercicios.DadosPessoa;
+
+public class Pessoa {
+    public String nome;
+    private int idade;
+    protected String endereco;
+    int telefone;
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    /*public void exibirPessoa() {
+        System.out.println("-------------------");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Endereco: " + endereco);
+        System.out.println("Telefone: " + telefone);
+    }*/
+}
