ff18930d94c8c324b75bcb1a43271fe738a376c3
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-10-20 12:22:48 -0300

    Serialization exemplo

---
+++ src/revisao/conceitos/serialization/Contato.java
@@ -0,0 +1,14 @@
+package revisao.conceitos.serialization;
+
+
+import java.io.Serializable;
+
+class Contato implements Serializable {
+    public String nome;
+    public String telefone;
+
+    public Contato(String initNome, String initTelefone) {
+        nome = initNome;
+        telefone = initTelefone;
+    }
+}
